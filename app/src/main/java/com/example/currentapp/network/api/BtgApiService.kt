eda945f5295a2c323a9447bc210b2652b963c3ea
Vitor Soares Silva <vitaoosp@gmail.com>
2021-05-30 22:33:48 -0300

    entrega do desafio

--- app/src/main/java/com/example/currentapp/network/api/BtgApiService.kt
+++ app/src/main/java/com/example/currentapp/network/api/BtgApiService.kt
@@ -4,6 +4,7 @@ import org.koin.core.KoinComponent
 import org.koin.core.get
 
 object BtgApiService : KoinComponent {
+
     private val builder = get<ApiBuilder>()
     val btgApi: EndPoint = builder.getBtgCliente().create(EndPoint::class.java)
 }
